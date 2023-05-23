3a14873d08b9c77b8f0817ba31b3d08010dde765
joasan2 <joasan2@emea.corpdir.net>
2023-05-23 17:02:27 +0100

    adding src

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 use raylib::prelude::*;
+use rand::Rng;
+
 
 fn main() {
+    let screen_width = 1920;
+    let screen_height = 1080;
+
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
+        .size(screen_width, screen_height)
         .title("Hello, World")
         .build();
+
      
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
+        let mut iteration = 1
+        
          
         d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+
+        while iteration < 101 {
+
+        }
+
+        d.draw_circle(0, 0, 50.0, Color::DARKBLUE);
     }
 }
