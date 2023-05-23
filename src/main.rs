11f71e6ce74aa6c3bd2455577543b01b0f6845f8
otretas <ofilipejorge@gmail.com>
2023-05-23 17:26:28 +0100

    added box

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,15 @@ fn main() {
         .size(640, 480)
         .title("rustsnake")
         .build();
-
+    rl.set_target_fps(60);
 
     while !rl.window_should_close() {
-        {
-            // Start Drawing
-            let mut d = rl.begin_drawing(&thread);
-            d.clear_background(Color::WHITE);
-            d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
-
-            if (d.is_mouse_button_down(MouseButton::MOUSE_LEFT_BUTTON))
-            {
-                d.draw_circle(d.get_mouse_x(), d.get_mouse_y(), 10.0, Color::BLUE)
-            }
-            if (d.is_mouse_button_down(MouseButton::MOUSE_RIGHT_BUTTON))
-            {
-                d.draw_circle(d.get_mouse_x(), d.get_mouse_y(), 10.0, Color::WHITE)
-            }
-        }
+        // Start Drawing
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::WHITE);
+        d.draw_text("Score: ", 12, 12, 20, Color::BLACK);
+        // d.draw_fps(12,12);
+        d.draw_rectangle(12, 30, d.get_screen_width()-24, d.get_screen_height()-50, Color::RED);
+        d.draw_rectangle(16, 34, d.get_screen_width()-32, d.get_screen_height()-58, Color::WHITE);
     }
 }
