ef842527016bb686079dc7a0e3e5cf3f307a800f
otretas <ofilipejorge@gmail.com>
2023-05-23 17:03:02 +0100

    added interaction

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,25 @@ use raylib::prelude::*;
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(640, 480)
-        .title("Hello, World")
+        .title("rustsnake")
         .build();
-     
+
+
     while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
-         
-        d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        {
+            // Start Drawing
+            let mut d = rl.begin_drawing(&thread);
+            d.clear_background(Color::WHITE);
+            d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+
+            if (d.is_mouse_button_down(MouseButton::MOUSE_LEFT_BUTTON))
+            {
+                d.draw_circle(d.get_mouse_x(), d.get_mouse_y(), 10.0, Color::BLUE)
+            }
+            if (d.is_mouse_button_down(MouseButton::MOUSE_RIGHT_BUTTON))
+            {
+                d.draw_circle(d.get_mouse_x(), d.get_mouse_y(), 10.0, Color::WHITE)
+            }
+        }
     }
 }
